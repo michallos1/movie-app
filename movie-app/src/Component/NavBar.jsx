9b313788b99f8b26358b9e531d903b8dfeb0df55
Michał Łoś <michallos1>
2022-06-29 11:25:10 +0200

    Update code

--- movie-app/src/Component/NavBar.jsx
+++ movie-app/src/Component/NavBar.jsx
@@ -1,28 +1,15 @@
-import React, { useState } from "react";
-import {Link} from "react-router-dom"
+import React from "react";
+
 
 
 function NavBar() {
-const [menuOn, setMenuOn] = useState(false)
-
 
 return(
 <nav className="Nav-bar">
-    <div className="Nav-mob" 
-          onClick={() => {setMenuOn(!menuOn);}
-          }>
-            <span></span>
-            <span></span>
-            <span></span>
-      </div>
-      <div className={menuOn ? "nav-menu-e" : "nav-menu"}>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/movies">Movies</Link></li>
-        <li><Link to="/series">Series</Link></li>
-      </ul>
-      </div>
-    </nav>
+  <h2>
+    Movies
+  </h2>
+  </nav>
 );
 }
 
--- movie-app/src/Component/movieQuery.js
+++ movie-app/src/Component/movieQuery.js
@@ -6,7 +6,8 @@ const MovieInfo = ({data}) => (
         data.map((item,i) =>
             <div key={i} className="item">
                 <div> <img alt={item.Titel}
-                 src={item.Poster === 'N/A' ? 'https://placehold.it/198x264&text=Image+Not+Found' : item.Poster}>
+                 src={item.Poster === 'N/A' ? 
+                 '' : item.Poster}>
                 </img></div>
                 <div>{item.Type}  </div>
                 <div className="year">{item.Year}  </div>
